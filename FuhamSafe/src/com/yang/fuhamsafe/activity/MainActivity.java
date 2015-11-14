76b11d0d34488d3a4e82dc1573b9d7c7b902efe8
FuhamYang <371317533@qq.com>
2015-11-14 19:33:25 +0800

    新建项目完成

---
+++ FuhamSafe/src/com/yang/fuhamsafe/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.yang.fuhamsafe.activity;
+
+import com.yang.fuhamsafe.R;
+import com.yang.fuhamsafe.R.layout;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+
+ 
+    
+}
